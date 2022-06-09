ed1d5f1e0207b048f8e24a75d979d2f1c1a2aa9c
Gabriel Martins <gabtec@sapo.pt>
2022-06-09 17:09:52 +0100

    Release v1.0.3

--- src/api/pt-districts/districts.routes.js
+++ src/api/pt-districts/districts.routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const DistrictsController = require("./districts.controller.js");
 
 // GET /v1/portugal/distritos
+// GET /v1/portugal/distritos?listar=nome|cod|abrev
 router.get("/", DistrictsController.getDistricts);
 // GET /api/v1/portugal/distritos/:id
 router.get("/:id", DistrictsController.getDistrictsByCode);
